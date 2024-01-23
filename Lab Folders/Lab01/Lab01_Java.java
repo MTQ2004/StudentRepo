0265282b9e6901c3851fb9a4b8716ed569b41c1e
MTQ2004 <146850945+MTQ2004@users.noreply.github.com>
2024-01-23 11:39:14 +0000

    Update Lab01_Java.java
    Changes made by student Mohammed Qayyum 220101044

--- Lab Folders/Lab01/Lab01_Java.java
+++ Lab Folders/Lab01/Lab01_Java.java
@@ -4,9 +4,9 @@ void testMethod1()
   {
   //First command x = y + z
   //Second commnad print(x)
+// Edit made by Mohammed Qayyum 220101044
 
-
-  //Last line print('Good bye')
+  //Last line print('TA TA')
 }
 
 void testMethod2()
